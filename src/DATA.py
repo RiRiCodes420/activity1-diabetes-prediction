1e88b1aac7624cadb5844080748c815add17ca19
Tinakumarj <tiinakumarj01@gmail.com>
2024-09-24 18:38:31 +0530

    Initial Commit

---
+++ src/DATA.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the diabetes dataset
+data = pd.read_csv(r'C:\Users\dheer\OneDrive\Desktop\ML\diabetes.csv')
+
+# Display the first few rows of the dataframe
+print(data.head())
