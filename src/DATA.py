609a5ffeebecdfabc59f5244cd3e694a80408214
Tinakumarj <tiinakumarj01@gmail.com>
2024-09-24 20:31:04 +0530

    Data

--- src/DATA.py
+++ src/DATA.py
@@ -1,5 +1,18 @@
+import gdown
 import pandas as pd
-data = pd.read_csv(r'C:\Users\HP\Desktop\FINAL\activity1-diabetes-prediction\diabetes.csv')
 
+# File ID extracted from the link
+file_id = "1UbQLrXi-TSLJw44307AZkWn2kdAuMo83"
+
+# The download URL
+url = f"https://drive.google.com/uc?id={file_id}"
+
+# Download the file using gdown
+output = 'Diabetes.csv'  # This is the filename it will save as
+gdown.download(url, output, quiet=False)
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(output)
+print(df.head())
 
 
--- src/Feature_engineering.py
+++ src/Feature_engineering.py
@@ -1,9 +1,10 @@
-# According to BMI, some ranges were determined and categorical variables were assigned.
-NewBMI = pd.Series(["Underweight", "Normal", "Overweight", "Obesity 1", "Obesity 2", "Obesity 3"], dtype = "category")
-df["NewBMI"] = NewBMI
-df.loc[df["BMI"] < 18.5, "NewBMI"] = NewBMI[0]
-df.loc[(df["BMI"] > 18.5) & (df["BMI"] <= 24.9), "NewBMI"] = NewBMI[1]
-df.loc[(df["BMI"] > 24.9) & (df["BMI"] <= 29.9), "NewBMI"] = NewBMI[2]
-df.loc[(df["BMI"] > 29.9) & (df["BMI"] <= 34.9), "NewBMI"] = NewBMI[3]
-df.loc[(df["BMI"] > 34.9) & (df["BMI"] <= 39.9), "NewBMI"] = NewBMI[4]
-df.loc[df["BMI"] > 39.9 ,"NewBMI"] = NewBMI[5]
+import pandas as pd
+from DATA import data
+NewBMI = pd.Series(["Underweight", "Normal", "Overweight", "Obesity 1", "Obesity 2", "Obesity 3"], dtype="category")
+data["NewBMI"] = NewBMI
+data.loc[data["BMI"] < 18.5, "NewBMI"] = NewBMI[0]
+data.loc[(data["BMI"] > 18.5) & (data["BMI"] <= 24.9), "NewBMI"] = NewBMI[1]
+data.loc[(data["BMI"] > 24.9) & (data["BMI"] <= 29.9), "NewBMI"] = NewBMI[2]
+data.loc[(data["BMI"] > 29.9) & (data["BMI"] <= 34.9), "NewBMI"] = NewBMI[3]
+data.loc[(data["BMI"] > 34.9) & (data["BMI"] <= 39.9), "NewBMI"] = NewBMI[4]
+data.loc[data["BMI"] > 39.9, "NewBMI"] = NewBMI[5]
